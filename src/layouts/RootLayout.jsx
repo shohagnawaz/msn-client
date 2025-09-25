d5a1d40cb486f4dcaaa2acd3db5064850d01407c
shohagnawaz <snawazdigital@gmail.com>
2025-09-25 16:28:16 +0600

    start

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import Navbar from "../pages/shared/Navbar/Navbar";
+
+const RootLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default RootLayout;
